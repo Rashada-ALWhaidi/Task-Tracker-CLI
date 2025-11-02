90fbbdd5a35f3b54c7e313d93f65454d2ef9209c
Rashada AL Whaidi <rashadaalwhaidi@gmail.com>
2025-11-02 14:09:20 +0200

    add Task

---
+++ main.js
@@ -0,0 +1,21 @@
+const http = require ('http');
+
+const server = http.createServer((req,res)=>{
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+    server.listen(300,()=>{
+        console.log(`server running on port :3000`);
+
+    })
