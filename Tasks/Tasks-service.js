370f7da5c1b0b8dd23d54d3c19924b92a79299a7
Rashada AL Whaidi <rashadaalwhaidi@gmail.com>
2025-11-04 15:14:44 +0200

    mark task as in progress

--- Tasks/Tasks-service.js
+++ Tasks/Tasks-service.js
@@ -124,3 +124,32 @@ export function listTasks() {
     console.log(`ID: ${task.id} | Description: ${task.description} | Status: ${task.status}`);
   });
 }
+
+
+export function markInProgress(id){
+  //To check if the file exists or not, if not we will create a file 
+  if (!fs.existsSync(path)) {
+    fs.writeFileSync(path, JSON.stringify([]));
+  }
+
+  // Reading the file content and converting it to an array
+  const data = fs.readFileSync(path, "utf-8");
+  const tasksArray = JSON.parse(data);
+
+ //Finding the task 
+    const markTask = tasksArray.find(tasks=> tasks.id === Number(id));
+    if(!markTask){
+      console.log("The Task Not Exist ! ");
+    }
+    
+ //Mark Task
+    markTask.status = "in progress";
+    markTask.updatedAt = new Date().toISOString();
+
+ //Adding the array to the file 
+   const jsonData =JSON.stringify(tasksArray, null, 2);
+   fs.writeFileSync(path,jsonData);
+    
+   console.log("Done");
+   
+}
